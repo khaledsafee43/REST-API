baae2f385e0119b5e31322af9a74807a49e5a0b9
Khaled Saifee <saifeekhaled1212@gmail.com>
2026-09-17 11:56:28 -0700

    The read User by id code maded successfully

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,40 @@
-const http = require("http");
+const exress = require("express");
+const app = exress();
 
-const server = http.createServer((req, res) => {
-  res.end("Serve is runing");
+app.use(exress.json());
+
+let users = [
+  {
+    id: 1,
+    name: "Ahmad",
+    email: "ahmad@gmail.com",
+    age: 22,
+  },
+  {
+    id: 2,
+    name: "Ali",
+    email: "ali@gmail.com",
+    age: 25,
+  },
+];
+
+app.get("/api/users", (req, res) => {
+  res.json(users);
 });
 
-server.listen(9000, () => {
+// api/users/:id
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => {
+    user.id === id;
+  });
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found",
+    });
+  }
+  res.status(200).json(user);
+});
+app.listen(9000, () => {
   console.log("server is runing on port 9000");
 });
